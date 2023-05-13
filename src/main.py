5f36a7dc659916c03684a7c110ce306b725caf50
NPhamDinh <49727803+NPhamDinh@users.noreply.github.com>
2023-05-13 03:22:32 +0200

    First Window with Tabs enabling switching between pages
    The window has tabs consisting each of a page/Widget.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-def window():
+from PyQt5.QtWidgets import (
+    QApplication, QDialog, QMainWindow, QMessageBox
+)
+from PyQt5.uic import loadUi
+
+from main_windows_ui import Ui_MainWindow
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 1500, 1000)
-    win.setWindowTitle("Applize - Application Insides")
-
+    win = Window()
     win.show()
-    sys.exit(app.exec_())
-
-window()
+    sys.exit(app.exec())
